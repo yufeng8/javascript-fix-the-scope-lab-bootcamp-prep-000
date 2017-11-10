5ffdffdbb90c7cf3acc3b544e8fa43e79e869680
Yufeng Liu Reach <yufeng8@vt.edu>
2017-11-10 18:37:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  
+
 }
 
 var funkyFunction = function() {
@@ -28,4 +28,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()
